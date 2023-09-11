bd02d51d5f58662241303ab86973671b7df9db79
gdurand2023 <131704538+gdurand2023@users.noreply.github.com>
2023-09-11 12:58:31 -0500

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,12 @@
+alert("Bienvenido a la Pagina de JavaScript, para poder mostrar los resultados presiona la tecla F12");
+//Ingresar Datos
+var num1 = parseInt(prompt("Ingrese el primer numero"));
+
+
+
+//Procesar Datos
+var resultado = num1 + 1;
+
+
+//Mostrar Datos
+console.log("El resultado es: " + resultado)
